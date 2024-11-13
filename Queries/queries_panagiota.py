8ea87dd392afe5c8f0c672492928e9a42c98e21f
panagiotagyft <giotagiota20001@gmail.com>
2024-11-13 02:16:37 +0200

    8

--- Queries/queries_panagiota.py
+++ Queries/queries_panagiota.py
@@ -16,36 +16,41 @@ conn = psycopg2.connect(
 )
 cursor = conn.cursor()
 
-# -- 2
-t='21:30:00'
-c=510
+# ------------------------------------------------------------------------------------------
+# -- 2:  Find the total number of reports per day for a specific “Crm Cd” and time range  --
+# ------------------------------------------------------------------------------------------
+t1='10:23:00'
+t2='10:26:00'
+c=623
 sql = """
     SELECT report.date_rptd, COUNT(report.dr_no)
-    FROM crime_report AS report
-    INNER JOIN crime_incident_crime_code AS code ON report.dr_no = code.dr_no 
-    WHERE report.crime_chronicle_time_occ = %s AND code.crm_cd = %s
+    FROM la_crimes.crime_report AS report
+    WHERE report.crime_type_crm_cd = %s AND 
+          report.crime_chronicle_time_occ BETWEEN %s AND %s 
     GROUP BY report.date_rptd
     ORDER BY report.date_rptd;
 """
 
 # Execute the query with parameters
-cursor.execute(sql, (t, c))
+cursor.execute(sql, (c, t1, t2))
 
 # Fetch results
 results = cursor.fetchall()
 for row in results:
-    print(f"{row[0]} -> {row[1]}")
+   print(f"{row[0]} -> {row[1]}")
 print(len(results))
 print()
 
 
-# -- 4
+# ---------------------------------------------------------------------------------------------------
+# -- 4:  Find the average number of crimes occurred per hour (24 hours) for a specific date range. --
+# ---------------------------------------------------------------------------------------------------
 start_date = '2023-01-01'
-end_date =  '2023-01-31'
+end_date =  '2023-01-05'
 sql = """
-SELECT count(cr.dr_no) / count(distinct cr.crime_chronicle_date_occ)
-FROM crime_report AS cr 
-WHERE cr.crime_chronicle_date_occ > %s and cr.crime_chronicle_date_occ < %s
+    SELECT count(report.dr_no) / count(distinct report.crime_chronicle_date_occ) AS avg_number_of_crimes
+    FROM la_crimes.crime_report AS report 
+    WHERE report.crime_chronicle_date_occ BETWEEN %s AND %s 
 """
 
 # Execute the query with parameters
@@ -57,84 +62,94 @@ for row in results:
     print(f"{row[0]}")
 print()
 
-# -- 6
-start_date = '2023-01-01'
-end_date =  '2023-01-31'
+
+#-------------------------------------------------------------------------------------------------
+# -- 6:??  Find the top-5 Area names with regards to total number of crimes reported per day for a
+#        specific date range. The same for Rpt Dist No.
+#-----------------------------------------------------------------------------------------------
+start_date = '2020-01-14'
+end_date =  '2020-01-15'
 
 sql="""
-WITH DailyCrimeCounts AS (
     SELECT 
         area_name,
-        cr.crime_chronicle_date_occ AS crime_date,
-        COUNT(*) AS daily_crimes
-    FROM crime_report AS cr
-    INNER JOIN area ON cr.area_area_id = area.area_id
-    WHERE cr.crime_chronicle_date_occ BETWEEN %s AND %s
-    GROUP BY area_name, cr.crime_chronicle_date_occ 
-)
-
-SELECT 
-    area_name, 
-    crime_date,
-    daily_crimes
-FROM 
-    DailyCrimeCounts
-ORDER BY 
-    daily_crimes DESC
-LIMIT 5;
+        COUNT(*) AS total_crimes
+    FROM la_crimes.crime_report 
+    INNER JOIN la_crimes.area ON area_area_id = area_id
+    WHERE date_rptd BETWEEN %s AND %s
+    GROUP BY area_name
+    ORDER BY total_crimes DESC
+    LIMIT 5;
 """
 cursor.execute(sql, (start_date, end_date))
 
 # Fetch results
 results = cursor.fetchall()
 for row in results:
-    print(f"{row[0]} {row[2]}")
+    print(f"{row[0]} {row[1]}")
 print()
 
 sql="""
-WITH DailyCrimeCounts AS (
-    SELECT 
-        reporting_district_rpt_dist_no AS rpt_dist_no,
-        cr.crime_chronicle_date_occ AS crime_date,
-        COUNT(*) AS daily_crimes
-    FROM crime_report AS cr
-    WHERE cr.crime_chronicle_date_occ BETWEEN %s AND %s
-    GROUP BY cr.reporting_district_rpt_dist_no, cr.crime_chronicle_date_occ 
-)
-
-SELECT 
-    rpt_dist_no, 
-    crime_date,
-    daily_crimes
-FROM 
-    DailyCrimeCounts
-ORDER BY 
-    daily_crimes DESC
-LIMIT 5;
+    SELECT reporting_district_rpt_dist_no AS rpt_dist_no,
+           COUNT(*) AS total_crimes
+    FROM la_crimes.crime_report 
+    WHERE date_rptd BETWEEN %s AND %s
+    GROUP BY reporting_district_rpt_dist_no
+    ORDER BY total_crimes DESC
+    LIMIT 5;
 """
 cursor.execute(sql, (start_date, end_date))
 
 # Fetch results
 results = cursor.fetchall()
 for row in results:
-    print(f"{row[0]} {row[2]}")
+    print(f"{row[0]} {row[1]}")
 print()
 
-## --8 
-#  ???
 
+# --------------------------------------------------------------------------------------------
+# -- 8: Find the second most common crime that has co-occurred with a particular crime for a  
+#      specific date range. 
+# --------------------------------------------------------------------------------------------
+crime = 948
+start_date = '2021-09-01'
+end_date =  '2025-10-16'
 
-# -- 10
+sql="""
+    SELECT report.crime_type_crm_cd AS snd_crime, COUNT(report.crime_type_crm_cd) as frequency
+    FROM la_crimes.crime_report AS report
+    WHERE report.crime_type_crm_cd <> %s AND 
+          (report.crime_chronicle_date_occ, report.crime_chronicle_time_occ) IN  
+                                  ( SELECT report.crime_chronicle_date_occ, report.crime_chronicle_time_occ
+                                    FROM la_crimes.crime_report AS report
+                                    WHERE report.crime_type_crm_cd = %s AND 
+                                          report.crime_chronicle_date_occ BETWEEN %s AND %s )
+    GROUP BY report.crime_type_crm_cd
+    ORDER BY frequency DESC
+    LIMIT 1 OFFSET 1;    
+"""
+
+cursor.execute(sql, (crime, crime, start_date, end_date))
+
+# Fetch results
+results = cursor.fetchall()
+for row in results:
+    print(f"{row[0]} {row[1]}")
+print()
+
+# --------------------------------------------------------------------------------------------
+# -- 10: Find the area with the longest time range without an occurrence of a specific crime. 
+#        Include the time range in the results. The same for Rpt Dist No.
+# --------------------------------------------------------------------------------------------
 c = 113
 sql = """
     -- Create a table with unique crime dates for each area
     WITH area_dates AS (
-        SELECT area_name, cr.crime_chronicle_date_occ AS date
-        FROM crime_report AS cr
-        INNER JOIN area ON cr.area_area_id = area.area_id
-        INNER JOIN crime_incident_crime_code AS code ON cr.dr_no = code.dr_no
-        WHERE code.crm_cd = %s  -- Filter for the specific crime code
-        GROUP BY area_name, cr.crime_chronicle_date_occ
+        SELECT area_name, crime_chronicle_date_occ AS date
+        FROM la_crimes.crime_report 
+        INNER JOIN la_crimes.area ON area_area_id = area_id
+        WHERE crime_type_crm_cd  = %s  
+        GROUP BY area_name, date
     )
 
     -- Final query to find the longest time gap without the specific crime
@@ -143,12 +158,10 @@ sql = """
         MIN(a2.date) AS end_date,  -- Earliest next date (end of the gap period)
         MIN(a2.date) - a1.date AS gap  -- Calculate the time gap
     FROM area_dates AS a1
-    INNER JOIN area_dates AS a2 
-        ON a1.area_name = a2.area_name  -- Join on the same area
-        AND a1.date < a2.date  -- Ensure that a2.date is after a1.date
+    INNER JOIN area_dates AS a2 ON a1.area_name = a2.area_name AND a1.date < a2.date  -- Ensure that a2.date is after a1.date
     GROUP BY a1.area_name, a1.date
-    ORDER BY gap DESC  -- Sort by the longest gap in descending order
-    LIMIT 1;  -- Return only the record with the longest gap
+    ORDER BY gap DESC  
+    LIMIT 1; 
 """
 cursor.execute(sql, (c,))
 
@@ -159,29 +172,22 @@ for row in results:
 print()
 
 c = 954
-sql = """
-    -- Create a table with unique crime dates for each reporting district
+sql="""
     WITH rpt_dist_dates AS (
-        SELECT cr.reporting_district_rpt_dist_no AS rpt_dist_no, cr.crime_chronicle_date_occ AS date
-        FROM crime_report AS cr
-        INNER JOIN crime_incident_crime_code AS code ON cr.dr_no = code.dr_no
-        WHERE code.crm_cd = %s  -- Filter for the specific crime code
+        SELECT reporting_district_rpt_dist_no AS rpt_dist_no, crime_chronicle_date_occ AS date
+        FROM la_crimes.crime_report 
+        WHERE crime_type_crm_cd  = %s  
         GROUP BY rpt_dist_no, date
     )
 
-    -- Final query to find the longest time gap without the specific crime
-    SELECT a1.rpt_dist_no, 
-           a1.date AS start_date,  -- Start of the gap period
-           MIN(a2.date) AS end_date,  -- Earliest next date (end of the gap period)
-           MIN(a2.date) - a1.date AS gap  -- Calculate the time gap
+    SELECT a1.rpt_dist_no, a1.date AS start_date,  MIN(a2.date) AS end_date, MIN(a2.date) - a1.date AS gap  
     FROM rpt_dist_dates AS a1
-    INNER JOIN rpt_dist_dates AS a2 
-        ON a1.rpt_dist_no = a2.rpt_dist_no  -- Join on the same rpt_dist_no
-        AND a1.date < a2.date  -- Ensure that a2.date is after a1.date
+    INNER JOIN rpt_dist_dates AS a2 ON a1.rpt_dist_no = a2.rpt_dist_no AND a1.date < a2.date  
     GROUP BY a1.rpt_dist_no, a1.date
-    ORDER BY gap DESC  -- Sort by the longest gap in descending order
-    LIMIT 1;  -- Return only the record with the longest gap
+    ORDER BY gap DESC  
+    LIMIT 1;
 """
+
 
 cursor.execute(sql, (c,))
 
@@ -191,25 +197,35 @@ for row in results:
     print(f"{row[0]} {row[1]} {row[2]} {row[3]}")
 print()
 
+# ------------------------------------------------------------------------------------------
+# -- 12:  Find the number of division of records for crimes reported on the same day in  
+#         different areas using the same weapon for a specific time range.
+# ------------------------------------------------------------------------------------------
 
-# -- 12
-t='00:01:00'
+start_time='00:00:00'
+end_time='23:59:00'
 sql="""
     WITH dr_date_weapon AS(
         SELECT dr_no, date_rptd, weapon_weapon_used_cd AS weapon_cd, area_area_id AS area_id
-        FROM crime_report 
-        JOIN crime_report_has_weapon ON dr_no = crime_report_dr_no
-        WHERE crime_chronicle_time_occ = %s
+        FROM la_crimes.crime_report 
+        JOIN la_crimes.crime_report_has_weapon ON dr_no = crime_report_dr_no
+        WHERE crime_chronicle_time_occ BETWEEN %s AND %s
         GROUP BY dr_no, date_rptd, weapon_weapon_used_cd, area_area_id
-        ORDER BY date_rptd, area_area_id, weapon_weapon_used_cd
+        ORDER BY date_rptd, weapon_weapon_used_cd, area_area_id
+    ),
+    RemoveDuplicates AS(
+        SELECT date_rptd, weapon_cd, area_id
+        FROM dr_date_weapon 
+        GROUP BY date_rptd, weapon_cd, area_id
+        HAVING COUNT(area_id) = 1
+        ORDER BY date_rptd, weapon_cd, area_id
     )
-    SELECT date_rptd, weapon_cd, COUNT(DISTINCT area_id) AS record_count
-    FROM dr_date_weapon 
+    SELECT date_rptd, weapon_cd, COUNT(*)
+    FROM RemoveDuplicates
     GROUP BY date_rptd, weapon_cd
-    ORDER BY date_rptd, weapon_cd;
 """
 
-cursor.execute(sql, (t,))
+cursor.execute(sql, (start_time,end_time))
 
 # Fetch results
 results = cursor.fetchall()
