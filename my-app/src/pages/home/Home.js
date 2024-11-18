18ce6faf56d23e488e75e4c5be6423469c7c1ff4
panagiotagyft <giotagiota20001@gmail.com>
2024-11-19 01:19:25 +0200

    home

--- my-app/src/pages/home/Home.js
+++ my-app/src/pages/home/Home.js
@@ -1,8 +1,34 @@
+import React, { useState } from "react";
+import "./Home.css"
 
 export default function Home() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  // Λειτουργία για εναλλαγή ενεργού στοιχείου
+  const toggleForm = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
-      <div>
-          this is home page
+    <div className="list">
+      {["Επιλογή 1", "Επιλογή 2", "Επιλογή 3"].map((title, index) => (
+        <div key={index} className="item">
+          <div className="title" onClick={() => toggleForm(index)}>
+            {title}
+          </div>
+          {activeIndex === index && (
+            <div className="form">
+              <form>
+                <label>Όνομα:</label>
+                <input type="text" />
+                <label>Email:</label>
+                <input type="email" />
+                <button type="submit">Υποβολή</button>
+              </form>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
