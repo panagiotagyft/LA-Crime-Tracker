81513f33cfdbb59d5bf06f9765b848dec17132f4
panagiotagyft <giotagiota20001@gmail.com>
2024-11-21 19:00:38 +0200

    home-queries

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
 import Home from './pages/home/Home';
+import Queries from './pages/queries/Queries';
+import Insert from './pages/insert/Insert';
+import Updates from './pages/updates/Updates';
 
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
   { path: "/home", element: <Home /> },
+  { path: "/queries", element: <Queries /> },
+  { path: "/insert", element: <Insert /> },
+  { path: "/updates", element: <Updates /> },
 ]);
 
 function App() {
--- my-app/src/pages/home/Home.js
+++ my-app/src/pages/home/Home.js
@@ -1,19 +1,6 @@
 import './Home.css';
-import Query1 from '../../components/query1/Query1';
-import Query2 from '../../components/query2/Query2';
-import Query3 from '../../components/query3/Query3';
-import Query4 from '../../components/query4/Query4';
-// 5 import Query5 from '../../components/query5/Query5';
-import Query6 from '../../components/query6/Query6';
-import Query7 from '../../components/query7/Query7';
-import Query8 from '../../components/query8/Query8';
-// import Query9 from '../../components/query9/Query9';
-import Query10 from '../../components/query10/Query10';
-// import Query11 from '../../components/query11/Query11';
-import Query12 from '../../components/query12/Query12';
-import Query13 from '../../components/query13/Query13';
-
-import UserNavbar from '../../components/navbar/UserNavbar'; 
+import UserNavbar from '../../components/navbar/UserNavbar';
+import { useNavigate } from 'react-router-dom';  
 
 export default function Home() {
   const userEmail = 'user@example.com';
@@ -22,30 +9,30 @@ export default function Home() {
     console.log('User logged out');
   };
 
-  const forms = [
-    <Query1 key="form1" />,
-    <Query2 key="form2" />,
-    <Query3 key="form3" />,
-    <Query4 key="form4" />,
-    // 5 <Query5 key="form5" />,
-    <Query6 key="form6" />,
-    <Query7 key="form7" />,
-    <Query8 key="form8" />,
-    // <Query9 key="form9" />,
-    <Query10 key="form10" />,
-    // <Query11 key="form11" />,
-    <Query12 key="form12" />,
-    <Query13 key="form13" />,
-  ];
+  const textOptions = [
+    { label: 'Queries', path: '/queries' },
+    { label: 'Insert', path: '/insert' },
+    { label: 'Updates', path: '/updates' },
+  ]; 
+
+  const handleOptionClick = (path) => {
+    navigate(path); // Ανακατεύθυνση στη συγκεκριμένη διαδρομή
+  };
+
+  const navigate = useNavigate(); // Hook για ανακατεύθυνση
 
   return (
     <div className='home'>
-      <UserNavbar userEmail={userEmail} onLogout={handleLogout} /> {/* Προσθήκη του Navbar */}
+      <UserNavbar userEmail={userEmail} onLogout={handleLogout} /> {/* Navbar */}
       <div className='homeWrapper'>
-        <div className='homeForm'> 
-          {forms.map((form, index) => (
-            <div key={index} style={{ marginBottom: '10px' }}>
-              {form}
+        <div className='homeText'>
+          <span>Welcome to the homepage!</span>
+          <h3>Please select one of the options below:</h3>
+        </div>
+        <div className='homeOptions'>
+          {textOptions.map((option, index) => (
+            <div key={index} className='circleOption' onClick={() => handleOptionClick(option.path)}>
+              <span className='circleText'>{option.label}</span> {/* Πρόσθεση className στο span */}
             </div>
           ))}
         </div>
