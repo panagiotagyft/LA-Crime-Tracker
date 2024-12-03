e2460c09479416028211bd18b821591355cc565a
Panagiota Gyftou <113244214+panagiotagyft@users.noreply.github.com>
2024-12-03 17:54:06 +0200

    Update query3_views.py

--- crime_backend/db_manager/views/queries_views/query3_views.py
+++ crime_backend/db_manager/views/queries_views/query3_views.py
@@ -19,56 +19,7 @@ class Query3View(APIView):
             with connection.cursor() as cursor:
 
                 sql="""
-                    WITH FlattenedCrimes AS (
-                        SELECT area_id, code.crm_cd AS crime_code
-                        FROM Crime_report AS report
-                        JOIN Crime_code AS code ON code.crm_cd_id = report.crm_cd
-                        JOIN Timestamp AS date ON date.timestamp_id = report.timestamp_id
-                        WHERE code.crm_cd != -1 AND date.date_occ = %s
-
-                        UNION ALL
-
-                        SELECT area_id, code.crm_cd AS crime_code
-                        FROM Crime_report AS report
-                        JOIN Crime_code AS code ON code.crm_cd_id = report.crm_cd_2
-                        JOIN Timestamp AS date ON date.timestamp_id = report.timestamp_id
-                        WHERE code.crm_cd != -1 AND date.date_occ = %s
-
-                        UNION ALL
-
-                        SELECT area_id, code.crm_cd AS crime_code
-                        FROM Crime_report AS report
-                        JOIN Crime_code AS code ON code.crm_cd_id = report.crm_cd_3
-                        JOIN Timestamp AS date ON date.timestamp_id = report.timestamp_id
-                        WHERE code.crm_cd != -1 AND date.date_occ = %s
-
-                        UNION ALL
-
-                        SELECT area_id, code.crm_cd AS crime_code
-                        FROM Crime_report AS report
-                        JOIN Crime_code AS code ON code.crm_cd_id = report.crm_cd_4
-                        JOIN Timestamp AS date ON date.timestamp_id = report.timestamp_id
-                        WHERE code.crm_cd != -1 AND date.date_occ = %s
-                    ),
-                    -- Step 2: Calculate the crime counts for each area and crime code
-                    CrimeCounts AS (
-                        SELECT 
-                            area_id,
-                            crime_code,
-                            COUNT(*) AS crime_count
-                        FROM FlattenedCrimes
-                        GROUP BY area_id, crime_code
-                        -- Group by area and crime code to get the number of occurrences
-                    )
-                    -- Step 3: Use DISTINCT ON to select the most common crime per area
-                    SELECT DISTINCT ON (area_id)
-                        area_id,
-                        crime_code,
-                        crime_count
-                    FROM CrimeCounts
-                    ORDER BY area_id, crime_count DESC;
-
-
+                   #....
                 """
                 cursor.execute(sql, [date, date, date, date])
                 rows = cursor.fetchall()
